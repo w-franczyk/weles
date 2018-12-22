b365cfadde961e70e1fa612a0611e07a7b3b9178
w <w@localhost.localdomain>
2018-12-22 19:05:56 +0000

    int 13 prawie dziala

--- stage1.asm
+++ stage1.asm
@@ -1,18 +1,32 @@
-start:
+_start:
+	;mov ah, 0x0A ; print character only
+	;mov al, 'W'
+	;mov bh, 0
+	;mov cx, 100
+	;int 0x10
+
+  ; Load stage 2
+  mov ah, 0x42 ; extended load
+  mov dl, 0x80 ; drive number, TO CHECK!
+  mov si, disk_address_packet
+  int 0x13
+  ; jmp 0x7e00
+ 
 	mov ah, 0x0A ; print character only
-	mov al, 'W'
+  mov edx, 0x7e00
+	mov al, [edx]
 	mov bh, 0
 	mov cx, 100
 	int 0x10
 
-  ; Load stage 2
-  mov ah, 0x42 ; extended load
-  mov dl, 0x80 ; drive number, to check
-  
-  
-  jmp exit
-
-
+disk_address_packet:
+  db 16 ; size
+  db 0 ; reserved
+  dw 10 ; blocks to copy, TODO: sprawdzic 128
+  stage2_address dd 0x7e00
+;  dq 1 ; start block
+  dd 1
+  dd 0
 
 test:
 	mov ah, 0x0A ; print character only
