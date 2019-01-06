afe64b4f9cb5f2722b4d844d8d3e9c2ea6168c0d
w <w@localhost.localdomain>
2019-01-06 21:13:09 +0000

    protected mode!

--- stage1.asm
+++ stage1.asm
@@ -1,39 +1,34 @@
-_start:
-	;mov ah, 0x0A ; print character only
-	;mov al, 'W'
-	;mov bh, 0
-	;mov cx, 100
-	;int 0x10
+%define STAGE1_BASE 0x7c00
+%define STAGE2_BASE 0x7e00
 
+[ORG STAGE1_BASE]
+[BITS 16]
+_start:
   ; Load stage 2
-  mov ah, 0x42 ; extended load
-  mov dl, 0x80 ; drive number, TO CHECK!
+  mov ah, 0x42 ; extended read
+  mov dl, 0x80 ; drive number, TODO: CHECK!
   mov si, disk_address_packet
   int 0x13
-  ; jmp 0x7e00
- 
-	mov ah, 0x0A ; print character only
-  mov edx, 0x7e00
-	mov al, [edx]
-	mov bh, 0
-	mov cx, 100
-	int 0x10
+  jc error ; something went wrong
+  jmp STAGE2_BASE
 
+; structures
 disk_address_packet:
   db 16 ; size
   db 0 ; reserved
-  dw 10 ; blocks to copy, TODO: sprawdzic 128
-  stage2_address dd 0x7e00
-;  dq 1 ; start block
-  dd 1
-  dd 0
+  dw 128 ; blocks to copy, max 65536 bytes in realmode
+  dd STAGE2_BASE
+  dq 1 ; start block
 
-test:
-	mov ah, 0x0A ; print character only
-	mov al, 'T'
-	mov bh, 0
-	mov cx, 100
-	int 0x10
-  ret
+; functions
+error: ; print 'E'
+  mov ah, 0x0A
+  mov al, 'E'
+  mov bh, 0
+  mov cx, 1
+  int 0x10
+  jmp exit
+ 
 
 exit:
+  jmp exit
--- stage2.asm
+++ stage2.asm
@@ -1,12 +1,106 @@
+%define STAGE2_BASE 0x7e00
+
+[ORG STAGE2_BASE]
+[BITS 16]
 start:
 	mov ah, 0x0A ; print character only
-	mov al, '2'
+	mov al, '3'
 	mov bh, 0
 	mov cx, 200
 	int 0x10
+
+  cli ; disable any other interrupts
+  xor ax, ax
+  mov ds, ax ; set data segment to 0
   
-  jmp exit
+  ; load GDT
+  lgdt [gdt_descriptor]
 
+  ; enable protected mode
+  mov eax, cr0
+  or eax, 1 ; enable the flag...
+  mov cr0, eax ; ...and save it back to cr0
+  jmp 0x8:protected_mode
+  
+[BITS 32]
+protected_mode:
+  ; set segment registers to data segment (0x10)
+  mov ax, 0x10
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  mov esp, 0x10000 ; move the stack pointer right after already loaded BIOS stuff
 
+  mov eax, 0xb8000
+  mov [eax], BYTE 'P'
+  mov eax, 0xb8001
+  mov [eax], BYTE 0x1b
+ 
+hang:
+  jmp hang
 
+; structures
+gdt:
+  gdt_null:
+  dq 0
+
+  gdt_code:
+  dw 0xffff ; limit 4GB TODO: detect memory sz
+  dw 0
+  db 0 ; base (start) address
+
+  ; access byte:
+  ; 1 present bit
+  ; 00 privilege bits, kernel level
+  ; 1 descriptor type, data/code
+  ; 1 executable bit, code
+  ; 0 direction bit, ring 0 only
+  ; 1 readable/writeable bit, code readable
+  ; 0 accessed bit
+  db 10011010b
+
+  ; limit & flags:
+  ; 1 granularity, 4kb granularity (segment size)
+  ; 1 size, 32bit protected mode
+  ; 00
+  ; 1111 limit continuation
+  db 11001111b
+
+  ; remaning base address:
+  db 0
+
+  gdt_data:
+  dw 0xffff ; limit. 4GB TODO: detect memory sz
+  dw 0
+  db 0 ; base (start) address. let's just make overapping data and code because the kernel is of course a safe piece of code :) and will rewrite the gdt anyway
+
+  ; access byte:
+  ; 1 present bit
+  ; 00 privilege bits, kernel level
+  ; 1 descriptor type, data/code
+  ; 0 executable bit, data
+  ; 0 direction bit, ring 0 only
+  ; 1 readable/writeable bit, data writeable
+  ; 0 accessed bit
+  db 10010010b
+
+  ; limit & flags:
+  ; 1 granularity, 4kb granularity (segment size)
+  ; 1 size, 32bit protected mode
+  ; 00
+  ; 1111 limit continuation
+  db 11001111b
+
+  ; remaning base address:
+  db 0
+gdt_end:
+
+gdt_descriptor:
+  dw gdt_end - gdt ; gdt size
+  dd gdt ; gdt address
+
+[BITS 32]
 exit:
+  jmp exit
